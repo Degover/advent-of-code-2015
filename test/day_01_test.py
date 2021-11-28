6f4c30f3991a58474e5555a0e2902a808e9aaffd
Degover <chaors2000@gmail.com>
2021-11-28 15:52:32 -0300

    Resolved day 01 challenge

--- test/day_01_test.py
+++ test/day_01_test.py
@@ -1,9 +1,41 @@
 import unittest
-from challenges.day_01 import increment
+from challenges.day_01 import *
 
-class Test_TestIncrementDecrement(unittest.TestCase):
-    def test_increment(self):
-        self.assertEqual(4, 4)
+test_cases_part1 =[
+        ("(())", 0),
+        ("()()", 0),
+        ("(((", 3),
+        ("(()(()(", 3),
+        ("))(((((", 3),
+        ("())", -1),
+        ("))(", -1),
+        (")))", -3),
+        (")())())", -3)
+    ]
 
-    def test_decrement(self):
-        self.assertEqual(increment(3), 4)
+test_cases_part2 =[
+        ("))(((((", 1),
+        ("())", 3),
+        ("))(", 1),
+        (")))", 1),
+        (")())())", 1),
+        (")", 1),
+        ("()())", 5)
+    ]    
+
+class Day01_Part01_Tests(unittest.TestCase):
+
+    def test_solution01_correctResults(self):
+        for test_case in test_cases_part1:
+            self.assertEqual(part_1_solution_01(test_case[0]), test_case[1])
+
+    def test_solution02_correctResults(self):
+        for test_case in test_cases_part1:
+            self.assertEqual(part_1_solution_02(test_case[0]), test_case[1])
+
+class Day01_Part02_Tests(unittest.TestCase):
+
+    def test_solution01_correctResults(self):
+        for test_case in test_cases_part2:
+            self.assertEqual(part_2_solution_01(test_case[0]), test_case[1])
+
