4c17e32604bd1f7fa0c2cc7ed2af084510bd8572
Degover <chaors2000@gmail.com>
2021-11-28 17:33:12 -0300

    Resolved day 02, part 1

---
+++ challenges/day_02.py
@@ -0,0 +1,51 @@
+# -- Part 01 solutions -- #
+class Part1_Solution01:
+    def run(self, raw_input):
+        total_sum = 0
+        for dimension_arr in self._parse_raw_input(raw_input):
+            faces_arr = self._calculate_faces_areas(dimension_arr)
+            smallest_face = faces_arr[0]
+
+            for face_size in faces_arr:
+                total_sum += face_size * 2
+
+                if(face_size < smallest_face):
+                    smallest_face = face_size
+
+            total_sum += smallest_face
+
+        return total_sum
+
+    def _parse_raw_input(self, raw_input):
+        current_number = ''
+        current_array = []
+        for char in raw_input:
+            if(char == 'x'):
+                current_array.append(int(current_number))
+                current_number = ''
+            elif(char == '\n'):
+                current_array.append(int(current_number))
+                yield current_array
+                current_number = ''
+                current_array = []
+            else:
+                current_number += char
+
+        current_array.append(int(current_number))
+        yield current_array
+
+    def _calculate_faces_areas(self, dimensions_array):
+        return [
+            dimensions_array[0] * dimensions_array[1],
+            dimensions_array[0] * dimensions_array[2],
+            dimensions_array[1] * dimensions_array[2]
+        ]
+
+# -- Part 02 solutions -- #
+
+
+
+if __name__ == '__main__':
+    raw_input = open('inputs/02.txt', 'r').read()
+
+    print(Part1_Solution01().run(raw_input))
--- test/day_02_test.py
+++ test/day_02_test.py
@@ -1,9 +1,38 @@
-# import unittest
-# from challenges.day_01 import increment
+import unittest
+from challenges.day_02 import *
 
-# class AnotherTEsts(unittest.TestCase):
-#     def test_increment(self):
-#         self.assertEqual(4, 4)
+class Day02_Part01_Tests(unittest.TestCase):
+    rawInput = f'''3x11x24
+13x5x19
+1x9x27
+24x8x21
+6x8x17
+19x18x22'''
+    parsedInput = [[3,11,24],[13,5,19],[1,9,27],[24,8,21],[6,8,17],[19,18,22]]
+    calculatedFaces = [
+        [33, 72, 264],
+        [65, 247, 95],
+        [9, 27, 243],
+        [192, 504, 168],
+        [48, 102, 136],
+        [342, 418, 396]
+    ]
 
-#     def test_decrement(self):
-#         self.assertEqual(increment(3), 4)
+    def test_parseRawInput_ShouldParse(self):
+        obj = Part1_Solution01()
+        index = 0
+        for parsedArr in obj._parse_raw_input(self.rawInput):
+            self.assertListEqual(parsedArr, self.parsedInput[index])
+            index += 1
+
+    def test_calculateFacesAreas_ShouldBeCorrect(self):
+        obj = Part1_Solution01()
+        for index in range(0, len(self.parsedInput)):
+            result = obj._calculate_faces_areas(self.parsedInput[index])
+            self.assertListEqual(result, self.calculatedFaces[index])
+
+    def test_runSolution_ShouldBeCorrect(self):
+        obj = Part1_Solution01()
+        self.assertEqual(obj.run(self.rawInput), 7387)
+
+
