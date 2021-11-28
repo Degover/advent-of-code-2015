92747b35d36875d589a93003baf42de26df89241
Degover <chaors2000@gmail.com>
2021-11-28 15:52:21 -0300

    Commented not used code

--- test/day_02_test.py
+++ test/day_02_test.py
@@ -1,9 +1,9 @@
-import unittest
-from challenges.day_01 import increment
+# import unittest
+# from challenges.day_01 import increment
 
-class AnotherTEsts(unittest.TestCase):
-    def test_increment(self):
-        self.assertEqual(4, 4)
+# class AnotherTEsts(unittest.TestCase):
+#     def test_increment(self):
+#         self.assertEqual(4, 4)
 
-    def test_decrement(self):
-        self.assertEqual(increment(3), 4)
+#     def test_decrement(self):
+#         self.assertEqual(increment(3), 4)
