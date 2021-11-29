fda8d9cf37600840b1b70df807510a9f01cb662d
Degover <chaors2000@gmail.com>
2021-11-29 18:30:05 -0300

    Resolved day 02, part 02

--- challenges/day_02.py
+++ challenges/day_02.py
@@ -1,8 +1,32 @@
+# -- Common -- #
+def parse_raw_input(raw_input):
+    current_number = ''
+    current_array = []
+
+    def append_number():
+        nonlocal current_array
+        nonlocal current_number
+        current_array.append(int(current_number))
+        current_number = ''
+
+    for char in raw_input:
+        if(char == 'x'):
+            append_number()
+        elif(char == '\n'):
+            append_number()
+            yield current_array
+            current_array = []
+        else:
+            current_number += char
+
+    append_number()
+    yield current_array
+
 # -- Part 01 solutions -- #
 class Part1_Solution01:
     def run(self, raw_input):
         total_sum = 0
-        for dimension_arr in self._parse_raw_input(raw_input):
+        for dimension_arr in parse_raw_input(raw_input):
             faces_arr = self._calculate_faces_areas(dimension_arr)
             smallest_face = faces_arr[0]
 
@@ -16,29 +40,6 @@ class Part1_Solution01:
 
         return total_sum
 
-    def _parse_raw_input(self, raw_input):
-        current_number = ''
-        current_array = []
-
-        def append_number():
-            nonlocal current_array
-            nonlocal current_number
-            current_array.append(int(current_number))
-            current_number = ''
-
-        for char in raw_input:
-            if(char == 'x'):
-                append_number()
-            elif(char == '\n'):
-                append_number()
-                yield current_array
-                current_array = []
-            else:
-                current_number += char
-
-        append_number()
-        yield current_array
-
     def _calculate_faces_areas(self, dimensions_array):
         return [
             dimensions_array[0] * dimensions_array[1],
@@ -47,10 +48,27 @@ class Part1_Solution01:
         ]
 
 # -- Part 02 solutions -- #
+class Part2_Solution01:
+    def run(self, raw_input):
+        total_sum = 0
+        for dimension_arr in parse_raw_input(raw_input):
+            total_sum += self._calculate_cubic_volume(dimension_arr)
+            total_sum += self._calculate_shortest_perimeter(dimension_arr)
 
+        return total_sum
+
+    def _calculate_shortest_perimeter(self, dimensions_array):
+        dimensions_array.sort()
+        [dim1, dim2, _] = dimensions_array
+        return 2*dim1 + 2*dim2
+
+    def _calculate_cubic_volume(self, dimensions_array):
+        return dimensions_array[0] *  dimensions_array[1] *  dimensions_array[2]
+        
 
 
 if __name__ == '__main__':
     raw_input = open('inputs/02.txt', 'r').read()
 
     print(Part1_Solution01().run(raw_input))
+    print(Part2_Solution01().run(raw_input))
--- test/day_02_test.py
+++ test/day_02_test.py
@@ -1,38 +1,71 @@
 import unittest
 from challenges.day_02 import *
 
-class Day02_Part01_Tests(unittest.TestCase):
-    rawInput = f'''3x11x24
+rawInput = f'''3x11x24
 13x5x19
 1x9x27
 24x8x21
 6x8x17
 19x18x22'''
-    parsedInput = [[3,11,24],[13,5,19],[1,9,27],[24,8,21],[6,8,17],[19,18,22]]
-    calculatedFaces = [
-        [33, 72, 264],
-        [65, 247, 95],
-        [9, 27, 243],
-        [192, 504, 168],
-        [48, 102, 136],
-        [342, 418, 396]
-    ]
+parsedInput = [
+    [3,11,24],
+    [13,5,19],
+    [1,9,27],
+    [24,8,21],
+    [6,8,17],
+    [19,18,22]
+]
+calculatedFaces = [
+    [33, 72, 264],
+    [65, 247, 95],
+    [9, 27, 243],
+    [192, 504, 168],
+    [48, 102, 136],
+    [342, 418, 396]
+]
+calculatedShortestPerimeters = [28, 36, 20, 58, 28, 74]
+calculatedCubicVolume = [792, 1235, 243, 4032, 816, 7524]
+
+class Day02_Part01_Tests(unittest.TestCase):
 
     def test_parseRawInput_ShouldParse(self):
-        obj = Part1_Solution01()
         index = 0
-        for parsedArr in obj._parse_raw_input(self.rawInput):
-            self.assertListEqual(parsedArr, self.parsedInput[index])
+        for parsedArr in parse_raw_input(rawInput):
+            self.assertListEqual(parsedArr, parsedInput[index])
             index += 1
 
     def test_calculateFacesAreas_ShouldBeCorrect(self):
         obj = Part1_Solution01()
-        for index in range(0, len(self.parsedInput)):
-            result = obj._calculate_faces_areas(self.parsedInput[index])
-            self.assertListEqual(result, self.calculatedFaces[index])
+        for index in range(0, len(parsedInput)):
+            result = obj._calculate_faces_areas(parsedInput[index])
+            self.assertListEqual(result, calculatedFaces[index])
 
     def test_runSolution_ShouldBeCorrect(self):
         obj = Part1_Solution01()
-        self.assertEqual(obj.run(self.rawInput), 7387)
+        self.assertEqual(obj.run(rawInput), 7387)
 
 
+class Day02_Part02_Tests(unittest.TestCase):
+
+    def test_parseRawInput_ShouldParse(self):
+        index = 0
+        for parsedArr in parse_raw_input(rawInput):
+            self.assertListEqual(parsedArr, parsedInput[index])
+            index += 1
+
+    def test_calculateShortestPerimeter_ShouldBeCorrect(self):
+        obj = Part2_Solution01()
+        for index in range(0, len(parsedInput)):
+            copied_list = parsedInput[index].copy()
+            result = obj._calculate_shortest_perimeter(copied_list)
+            self.assertEqual(result, calculatedShortestPerimeters[index])
+
+    def test_calculateCubicVolume_ShouldBeCorrect(self):
+        obj = Part2_Solution01()
+        for index in range(0, len(parsedInput)):
+            result = obj._calculate_cubic_volume(parsedInput[index])
+            self.assertEqual(result, calculatedCubicVolume[index])
+
+    def test_runSolution_ShouldBeCorrect(self):
+        obj = Part2_Solution01()
+        self.assertEqual(obj.run(rawInput), 14886)
