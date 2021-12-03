edb1b855edd40f5f451c3f91f2e3b220ef27e9c6
Degover <chaors2000@gmail.com>
2021-12-03 18:10:37 -0300

    Advancements on day 06 answer

--- challenges/day_06.py
+++ challenges/day_06.py
@@ -1,28 +1,81 @@
 import re as regex
 
-class LightGrid:
-
-    def __init__(self):
-        self.grid = [ [ False for _ in range (1000) ] for _ in range (1000) ]
-
-    def execute_command(self, command, coor_a, coor_b):
-        lambda_fn = None
-        if command == 'toggle':
-            lambda_fn = lambda state: not state
-        elif command == 'turn on':
-            lambda_fn = lambda _: True
-        else: # turn off
-            lambda_fn = lambda _: False
-        
-        for x in range(coor_a[0], coor_b[0] + 1):
-            for y in range(coor_a[1], coor_b[1] + 1):
-                isLightOn = self.grid[x][y]
-                self.grid[x][y] = lambda_fn(isLightOn)
-
-    def read_file_input(file_input):
+class LightInstructionInterpreter:
+    
+    def read_file_input(self, file_input):
         coord_regex = regex.compile('(\d+),(\d+) through (\d+),(\d+)')
 
         for line in file_input:
             match = coord_regex.search(line)
-            coord_a = match.group(1, 2)
-            coord_b = match.group(3, 4)
+            coord_a = tuple((int(i) for i in match.group(1, 2)))
+            coord_b = tuple((int(i) for i in match.group(3, 4)))
+
+            if line.startswith('toggle'):
+                self.toggle_lights(coord_a, coord_b)
+            elif line.startswith('turn on'):
+                self.turn_on_lights(coord_a, coord_b)
+            else:
+                self.turn_off_lights(coord_a, coord_b)
+
+    def toggle_lights(self, coord_a, coord_b): pass
+    def turn_on_lights(self, coord_a, coord_b): pass
+    def turn_off_lights(self, coord_a, coord_b): pass
+
+class LightGrid(LightInstructionInterpreter):
+
+    def __init__(self):
+        self.grid = [ [ False for _ in range (1000) ] for _ in range (1000) ]
+
+    def execute_command(self, command_fn, coor_a, coor_b):
+        for x in range(coor_a[0], coor_b[0] + 1):
+            for y in range(coor_a[1], coor_b[1] + 1):
+                isLightOn = self.grid[x][y]
+                self.grid[x][y] = command_fn(isLightOn)
+
+    def toggle_lights(self, coord_a, coord_b):
+        self.execute_command(lambda state: not state, coord_a, coord_b)
+
+    def turn_on_lights(self, coord_a, coord_b):
+        self.execute_command(lambda _: True, coord_a, coord_b)
+
+    def turn_off_lights(self, coord_a, coord_b):
+        self.execute_command(lambda _: False, coord_a, coord_b)
+
+class BrightnessCalculator(LightInstructionInterpreter):
+
+    def __init__(self):
+        self.total_brightness = 0
+
+    def increase_brightness(self, quantity, coord_a, coord_b):
+        diff_x = coord_b[0] - coord_a[0] + 1
+        diff_y = coord_b[1] - coord_a[1] + 1
+        self.total_brightness += quantity * (diff_x * diff_y)
+
+    def toggle_lights(self, coord_a, coord_b):
+        self.increase_brightness(2, coord_a, coord_b)
+
+    def turn_on_lights(self, coord_a, coord_b):
+        self.increase_brightness(1, coord_a, coord_b)
+
+    def turn_off_lights(self, coord_a, coord_b):
+        self.increase_brightness(-1, coord_a, coord_b)
+
+def part1_solution(file_input):
+    light_grid = LightGrid()
+    light_grid.read_file_input(file_input)
+
+    result = sum([ sum([ 1 for isOn in array if isOn ]) for array in light_grid.grid ])
+    return result
+
+def part2_solution(file_input):
+    calculator = BrightnessCalculator()
+    calculator.read_file_input(file_input)
+
+    return calculator.total_brightness
+
+if __name__ == '__main__':
+    with open('inputs/06.txt', 'r') as file:
+        print(part1_solution(file))
+
+    with open('inputs/06.txt', 'r') as file:
+        print(part2_solution(file))
--- test/day_06_test.py
+++ test/day_06_test.py
@@ -1,11 +1,11 @@
 import unittest
 from challenges.day_06 import *
 
-class Day_06_Tests(unittest.TestCase):
+class Day06_Tests(unittest.TestCase):
 
-    def testLightGrid_ExecuteCommand_WithTurnOnCommandAndAllLightsOff_ShouldTurnOn(self):
+    def testLightGrid_TurnOnLights_WithAllLightsOff_ShouldTurnOn(self):
         light_grid = LightGrid()
-        light_grid.execute_command('turn on', (0, 0), (2, 2))
+        light_grid.turn_on_lights((0, 0), (2, 2))
 
         for x, row in enumerate(light_grid.grid):
             for y, isLightOn in enumerate(row):
@@ -15,19 +15,19 @@ class Day_06_Tests(unittest.TestCase):
                 else:
                     self.assertTrue(x not in [0, 1, 2] or y not in [0, 1, 2])
         
-    def testLightGrid_ExecuteCommand_WithTurnOnCommandAndAllLightsOn_ShouldDoNothing(self):
+    def testLightGrid_TurnOnLights_WithAllLightsOn_ShouldDoNothing(self):
         light_grid = LightGrid()
         light_grid.grid = [ [ True for _ in range (1000) ] for _ in range (1000) ]
-        light_grid.execute_command('turn on', (0, 0), (2, 2))
+        light_grid.turn_on_lights((0, 0), (2, 2))
 
         for row in light_grid.grid:
             for isLightOn in row:
                 self.assertTrue(isLightOn)
         
-    def testLightGrid_ExecuteCommand_WithTurnOffCommandAndAllLightsOn_ShouldTurnOff(self):
+    def testLightGrid_TurnOffLights_WithAllLightsOn_ShouldTurnOff(self):
         light_grid = LightGrid()
         light_grid.grid = [ [ True for _ in range (1000) ] for _ in range (1000) ]
-        light_grid.execute_command('turn off', (0, 0), (2, 2))
+        light_grid.turn_off_lights((0, 0), (2, 2))
 
         for x, row in enumerate(light_grid.grid):
             for y, isLightOn in enumerate(row):
@@ -37,17 +37,17 @@ class Day_06_Tests(unittest.TestCase):
                 else:
                     self.assertTrue(x not in [0, 1, 2] or y not in [0, 1, 2])
 
-    def testLightGrid_ExecuteCommand_WithTurnOffCommandAndAllLightsOff_ShouldDoNothing(self):
+    def testLightGrid_TurnOffLights_WithAllLightsOff_ShouldDoNothing(self):
         light_grid = LightGrid()
-        light_grid.execute_command('turn off', (0, 0), (2, 2))
+        light_grid.turn_off_lights((0, 0), (2, 2))
 
         for row in light_grid.grid:
             for isLightOn in row:
                 self.assertFalse(isLightOn)
     
-    def testLightGrid_ExecuteCommand_WithToggleCommandAndAllLightsOff_ShouldTurnOn(self):
+    def testLightGrid_ToogleLights_WithAllLightsOff_ShouldTurnOn(self):
         light_grid = LightGrid()
-        light_grid.execute_command('toggle', (0, 0), (2, 2))
+        light_grid.toggle_lights((0, 0), (2, 2))
 
         for x, row in enumerate(light_grid.grid):
             for y, isLightOn in enumerate(row):
@@ -57,10 +57,10 @@ class Day_06_Tests(unittest.TestCase):
                 else:
                     self.assertTrue(x not in [0, 1, 2] or y not in [0, 1, 2])
         
-    def testLightGrid_ExecuteCommand_WithToggleCommandAndAllLightsOn_ShouldTurnOff(self):
+    def testLightGrid_ToogleLights_WithAllLightsOn_ShouldTurnOff(self):
         light_grid = LightGrid()
         light_grid.grid = [ [ True for _ in range (1000) ] for _ in range (1000) ]
-        light_grid.execute_command('toggle', (0, 0), (2, 2))
+        light_grid.toggle_lights((0, 0), (2, 2))
 
         for x, row in enumerate(light_grid.grid):
             for y, isLightOn in enumerate(row):
@@ -80,12 +80,73 @@ class Day_06_Tests(unittest.TestCase):
         
     def testLightGrid_ReadFileInput_WithMultipleCommands_ShouldBeCorrect(self):
         light_grid = LightGrid()
-        commands = ['turn on 0,0 through 999,999']
+        commands = [
+            'turn on 0,0 through 5,5',
+            'toggle 3,0 through 5,5',
+            'turn off 0,3 through 5,5'
+        ]
         light_grid.read_file_input(commands)
 
-        for row in light_grid.grid:
-            for isLightOn in row:
-                self.assertTrue(isLightOn)
+        for x, row in enumerate(light_grid.grid):
+            for y, isLightOn in enumerate(row):
+                if(isLightOn):
+                    self.assertIn(x, [0, 1, 2])
+                    self.assertIn(y, [0, 1, 2])
+                else:
+                    self.assertTrue(x not in [0, 1, 2] or y not in [0, 1, 2])
+
+    def testBrightnessCalculator_IncreaseBrightness_WithPositiveQuantity_ShouldIncrease(self):
+        calculator = BrightnessCalculator()
+        calculator.increase_brightness(1, (0, 0), (3, 3))
+
+        self.assertEqual(calculator.total_brightness, 16)
+
+    def testBrightnessCalculator_IncreaseBrightness_WithNegativeQuantity_ShouldDecrease(self):
+        calculator = BrightnessCalculator()
+        calculator.increase_brightness(-1, (0, 0), (3, 3))
+
+        self.assertEqual(calculator.total_brightness, -16)
+
+    def testPart1_RunSolution_WithNormalCommands_ShouldBeCorrect(self):
+        commands = [
+            'turn on 0,0 through 999,999',
+            'toggle 0,0 through 999,0',
+            'turn off 499,499 through 500,500'
+        ]
+        expected_output = 998996
+        output = part1_solution(commands)
+
+        self.assertEqual(output, expected_output)
         
+    def testPart1_RunSolution_WithOverllapingCommands_ShouldBeCorrect(self):
+        commands = [
+            'turn on 0,0 through 5,5',
+            'toggle 3,0 through 5,5',
+            'turn off 0,3 through 5,5'
+        ]
+        expected_output = 9
+        output = part1_solution(commands)
+
+        self.assertEqual(output, expected_output)
         
+    def testPart2_RunSolution_WithSingleCommands_ShouldBeCorrect(self):
+        test_cases = [
+            ('turn on 0,0 through 0,0', 1),
+            ('turn off 0,0 through 1,2', -6),
+            ('toggle 0,0 through 999,999', 2000000)
+        ]
+        for input, expected_out in test_cases:
+            output = part2_solution([ input ])
+            self.assertEqual(output, expected_out)
+
+    def testPart2_RunSolution_WithOverllapingCommands_ShouldBeCorrect(self):
+        commands = [
+            'turn on 0,0 through 5,5',
+            'toggle 3,0 through 5,5',
+            'turn off 0,3 through 5,5'
+        ]
+        expected_output = 54
+        output = part2_solution(commands)
+
+        self.assertEqual(output, expected_output)
         
